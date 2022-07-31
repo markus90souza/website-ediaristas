59a542304c44d6915528c17bdbd46ba1960b84e4
markus90souza <markus90souza@gmail.com>
2022-07-30 21:54:27 -0300

    Setup project and add MUI and set theme

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+  
+
+    <Button variant={'contained'} >
+      OK
+    </Button>
+  )
+}
+
+export default Home
