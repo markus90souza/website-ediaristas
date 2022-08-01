4726a497c706f3aad833198b799c1712c4015f4d
markus90souza <markus90souza@gmail.com>
2022-07-31 22:02:51 -0300

    create link  component

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Link }  from '.';
+
+export default {
+    title: 'Components/Link',
+    component: Link,
+} as ComponentMeta<typeof Link>;
+
+const Template: ComponentStory<typeof Link> = (args) => <Link {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    children: 'Clique Aqui',
+    href: '/#',
+};
+
+
